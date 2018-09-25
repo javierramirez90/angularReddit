83ad83322d6faca13c468e9d2092312542483773
Javier Ramírez <javier.ramirez@yogome.com>
2018-09-25 16:46:51 -0500

    Model and functional article post

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+  // article: Article;
+
+  constructor() {
+    // article is populated by the Input now,
+    // so we don't need anything here
+    
+    // this.article = new Article(
+    //   'Angular 2',
+    //   'http://angular.io',
+    //   10);
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+  ngOnInit() {
+  }
+
+}
